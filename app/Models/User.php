7ddab7fbe1d98bfa2b20ff2735276382eac0468b
devano <elazzouziadnan4@gmil.com>
2026-03-04 20:50:08 +0000

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,6 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-    // App\Models\User.php
     public function colocation()
     {
         return $this->belongsToMany(Colocation::class, 'amitiés', 'user_id', 'colocation_id')
